9c36569a8b699d1941421f8a6917eb979bc7c385
giovanniaravena <giovanni.aravena.13@sansano.usm.cl>
2022-07-22 15:40:57 -0400

    def build

--- vars/policyScanSandboxLib.groovy
+++ vars/policyScanSandboxLib.groovy
@@ -1,5 +1,5 @@
 def uploadArtifact(Map params){
-  withCredentials([string(credentialsId: 'b42d9a0dc0502a2c2ac0f19a8d9d8bf9', variable: 'SecretCredentialsVeracode')]) {
-    veracode applicationName: "${params.ApplicationName}", canFailJob: false, debug: true, deleteIncompleteScan: true, fileNamePattern: '', replacementPattern: '', createSandbox: false, sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '' ,scanName: "${params.currentDate}", teams: '', timeout: 1, uploadIncludesPattern: "${params.UploadIncludesPattern}", vid: "b42d9a0dc0502a2c2ac0f19a8d9d8bf9", vkey: "${SecretCredentialsVeracode}", waitForScan: false
+  withCredentials([string(credentialsId: "${params.Id}", variable: 'SecretCredentialsVeracode')]) {
+    veracode applicationName: "${params.ApplicationName}", canFailJob: false, debug: true, deleteIncompleteScan: true, fileNamePattern: '', replacementPattern: '', createSandbox: false, sandboxName: '', scanExcludesPattern: '', scanIncludesPattern: '' ,scanName: "${params.currentDate}", teams: '', timeout: 1, uploadIncludesPattern: "${params.UploadIncludesPattern}", vid: "${params.Id}", vkey: "${SecretCredentialsVeracode}", waitForScan: false
   }
 }
