b4d5b141dc769b13b015cbbbb1d421d19cce90f3
giovanniaravena <giovanni.aravena.13@sansano.usm.cl>
2022-07-27 11:44:47 -0400

    pipelineScan

--- vars/pipelineScanLib.groovy
+++ vars/pipelineScanLib.groovy
@@ -5,11 +5,11 @@ def scanArtifact(Map params){
     sh "java -jar pipeline-scan.jar \
         --veracode_api_id ${params.Id} \
         --veracode_api_key ${SecretCredentialsVeracode} \
-        --file ${params.file} \
+        --file '${params.file}' \
         --fail_on_severity='Very High, High' \
         --fail_on_cwe='80' \
         --timeout '3' \
-        --project_name ${params.ApplicationName} "
+        --project_name '${params.ApplicationName}' "
   }
 }
 
