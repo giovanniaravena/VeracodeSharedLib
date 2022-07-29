4c339906e0d656573bdb42951d7bec3222795638
giovanniaravena <giovanni.aravena.13@sansano.usm.cl>
2022-07-29 18:58:41 -0400

    SCA agent-based

--- vars/sca.groovy
+++ vars/sca.groovy
@@ -9,7 +9,6 @@ def scanProject(Map params){
         export SCAN_DIR=.
         touch SCA_Results_Build_${BUILD_NUMBER}.txt
         curl -sSL https://download.sourceclear.com/ci.sh | bash -s -- scan --update-advisor 2>&1 | tee SCA_Results_Build_${BUILD_NUMBER}.txt
-        ! grep -E 'CVE-2021-45046|CVE-2021-22118' SCA_Results_Build_${BUILD_NUMBER}.txt
     '''
     // sh '''
     //     export SCAN_DIR=${params.file}
